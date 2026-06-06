84c14c90851537f9a0a945ba2b3a5853a09e8ba0
Ivan Glushenkov <glushman966@gmail.com>
2026-06-06 18:58:22 +0300

    create CourseViewSet

--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from materials.models import Course
+from .serializers import CourseSerializer
+
+
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
