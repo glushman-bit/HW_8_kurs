499ea41f95cd31b035aebfae2e696058117244a5
Ivan Glushenkov <glushman966@gmail.com>
2026-06-06 18:40:00 +0300

    add app users, create model User

---
+++ users/models.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True,
+        verbose_name="email",
+    )
+    phone = models.CharField(
+        max_length=25,
+        verbose_name="Номер телефона",
+        blank=True,
+        null=True,
+        help_text="Введите номер телефона",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatar",
+        verbose_name="Аватар",
+        blank=True,
+        null=True,
+        help_text="Загрузить аватар"
+    )
+    city = models.CharField(
+        verbose_name="Город",
+        help_text="Введите город",
+        blank=True,
+        null=True,
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
