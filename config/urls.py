a93c38d89a9cd1d8a2c6795adc7d4572102192bd
Ivan Glushenkov <glushman966@gmail.com>
2026-06-06 19:52:07 +0300

    create UserSerializer, UserViewSet and urls for users

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('users/', include('users.urls'), name='users'),
+    path('users/', include('users.urls'), name='users'),
     path('materials/', include('materials.urls', namespace='materials')),
 ]
