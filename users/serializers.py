fcc0a82f0b760dce3276df79ce89648fc562184d
Ivan Glushenkov <glushman966@gmail.com>
2026-06-08 00:10:04 +0300

    create PaymentListAPIView, urls, PaymentSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework.serializers import ModelSerializer
 
-from .models import User
+from .models import User, Payment
 
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
+        fields = ("id", "email", "phone", "avatar", "city",)
+
+
+class PaymentSerializer(ModelSerializer):
+    """ Сериализатор платежей """
+    class Meta:
+        model = Payment
         fields = "__all__"
--- users/views.py
+++ users/views.py
@@ -1,9 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 
-from users.models import User
+from users.serializers import PaymentSerializer
+from users.models import User, Payment
 from users.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class PaymentListAPIView(generics.ListAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
