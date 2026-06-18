a3fa869f0ee3d53af538f345f3c133268e221441
Ivan Glushenkov <glushman966@gmail.com>
2026-06-18 19:41:52 +0300

    add new fields in users/models.py

--- users/models.py
+++ users/models.py
@@ -71,6 +71,20 @@ class Payment(models.Model):
         verbose_name="Оплаченный урок",
     )
     amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Сумма оплаты")
+    session_id = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        verbose_name="ID сессии",
+        help_text="Укажите ID сессии"
+    )
+    link = models.URLField(
+        max_length=600,
+        blank=True,
+        null=True,
+        verbose_name="Ссылка на оплату",
+        help_text="Укажите ссылку на оплату"
+    )
     payment_method = models.CharField(max_length=20, choices=PAYMENT_METHODS, verbose_name="Способ оплаты")
 
     class Meta:
