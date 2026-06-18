66cfab00817ec83a39377782af580ba70f48b74b
Ivan Glushenkov <glushman966@gmail.com>
2026-06-19 01:50:48 +0300

    add url in urls.py

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import SimpleRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import PaymentListAPIView, UserCreateAPIView, UserViewSet
+from users.views import PaymentListAPIView, UserCreateAPIView, UserViewSet, PaymentCreateAPIView
 
 app_name = UsersConfig.name
 
@@ -13,6 +13,7 @@ router.register('', UserViewSet)
 
 urlpatterns = [
     path('payments/', PaymentListAPIView.as_view(), name='payments-list'),
+    path('payments/create/', PaymentCreateAPIView.as_view(), name='payment-create'),
     path('register/', UserCreateAPIView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
