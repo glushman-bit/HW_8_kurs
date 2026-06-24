be14790a9bc0c7592c8f37d25b71ee3631aaf1fd
Ivan Glushenkov <glushman966@gmail.com>
2026-06-24 23:31:38 +0300

    update settings.py for send mail

--- config/settings.py
+++ config/settings.py
@@ -178,3 +178,13 @@ CELERY_BEAT_SCHEDULE = {
         "schedule": timedelta(),
     },
 }
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", False) == "True"
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", False) == "True"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
