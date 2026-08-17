7ce2ea411be96b3a1e64e0f2c44101fcf38486b8
Ivan Glushenkov <glushman966@gmail.com>
2026-08-17 20:10:21 +0300

    upgrade README.md and ci.yml

--- config/settings.py
+++ config/settings.py
@@ -21,11 +21,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [
-    '89.169.178.35',
-    'localhost',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = ['*']
 
 HOST_URL = 'http://127.0.0.1:8000/'
 
